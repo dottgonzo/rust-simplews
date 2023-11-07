74914dc2359f5549ad325fa37c103de44a3e6113
dottg0n20 <dev@dariocaruso.info>
2023-11-07 01:54:43 +0100

    ref

---
+++ build-all.sh
@@ -0,0 +1,6 @@
+cargo build --release --target=aarch64-unknown-linux-gnu
+cargo build --release --target=aarch64-unknown-linux-musl
+cargo build --release --target=x86_64-unknown-linux-gnu
+cargo build --release --target=x86_64-unknown-linux-musl
+cargo build --release --target=x86_64-pc-windows-gnu
+cargo build --release --target=wasm32-wasi-preview1-threads
---
+++ check-all.sh
@@ -0,0 +1,6 @@
+cargo check --target=aarch64-unknown-linux-gnu
+cargo check --target=aarch64-unknown-linux-musl
+cargo check --target=x86_64-unknown-linux-gnu
+cargo check --target=x86_64-unknown-linux-musl
+cargo check --target=x86_64-pc-windows-gnu
+# cargo check --target=wasm32-wasi-preview1-threads
