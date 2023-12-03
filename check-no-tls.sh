98434df43a1e3984264ca022ac2ef4750c5f7f23
dottg0n20 <dev@dariocaruso.info>
2023-12-03 15:36:55 +0100

    remove wasm

--- check-no-tls.sh
+++ check-no-tls.sh
@@ -4,4 +4,4 @@ cargo check --release --no-default-features --target=x86_64-unknown-linux-gnu
 cargo check --release --no-default-features --target=x86_64-unknown-linux-musl
 cargo check --release --no-default-features --target=x86_64-pc-windows-gnu
 # cargo check --release --no-default-features --target=x86_64-android-linux-gnu
-cargo check --release --no-default-features --target=wasm32-wasi-preview1-threads
+# cargo check --release --no-default-features --target=wasm32-wasi-preview1-threads
