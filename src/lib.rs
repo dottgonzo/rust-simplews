05e057ed9a068122ce2b72957da77c38b5680004
dottg0n20 <dev@dariocaruso.info>
2024-06-11 23:12:47 +0200

    all signature schemes

--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,13 @@ impl rustls::client::danger::ServerCertVerifier for NoVerifier {
             rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
             rustls::SignatureScheme::RSA_PKCS1_SHA384,
             rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
+            rustls::SignatureScheme::RSA_PKCS1_SHA512,
+            rustls::SignatureScheme::ECDSA_NISTP521_SHA512,
+            rustls::SignatureScheme::RSA_PSS_SHA256,
+            rustls::SignatureScheme::RSA_PSS_SHA384,
+            rustls::SignatureScheme::RSA_PSS_SHA512,
+            rustls::SignatureScheme::ED25519,
+            rustls::SignatureScheme::ED448,
         ]
     }
 }
